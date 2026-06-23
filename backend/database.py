ceb5a4003f45ed3827181cd8e17d3f34c6b27075
Amaravathi-Lokesh <amaravathilokesh2005@gmail.com>
2026-06-23 16:14:38 +0530

    debug start

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from sqlalchemy import create_engine
 from config.settings import Settings
 DATABASE_URL = Settings.db_key
-
+print("DATABASE_URL =", DATABASE_URL)
 engine = create_engine(
     DATABASE_URL,
     # connect_args={"check_same_thread": False}
