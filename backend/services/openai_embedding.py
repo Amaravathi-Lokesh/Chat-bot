0b5ffd29acc87f6c1a4925e54a227bf0b1d6d4f7
Amaravathi-Lokesh <amaravathilokesh2005@gmail.com>
2026-06-25 12:38:27 +0530

    openai to genai

--- backend/services/openai_embedding.py
+++ backend/services/openai_embedding.py
@@ -8,7 +8,7 @@ client = genai.Client(
 def create_embedding(text):
 
     response = client.models.embed_content(
-        model="text-embedding-002",
+        model="text-embedding-001",
         contents=text
     )
 
